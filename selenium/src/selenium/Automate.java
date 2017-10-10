8caae6f1fc36736e1defe2cf80b363bd3794d5ae
VivekKumarSingh <VivekKumarSingh@D32WS6C2.corp.fairisaac.com>
2017-10-10 23:19:36 +0530

    finaally

--- selenium/src/selenium/Automate.java
+++ selenium/src/selenium/Automate.java
@@ -1,109 +1,94 @@
 package selenium;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//comment the above line and uncomment below line to use Chrome
-//import org.openqa.selenium.chrome.ChromeDriver;
 public class Automate {
 
 
     public static void main(String[] args) throws InterruptedException {
     	
-    	//System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
-		//System.setProperty("webdriver.gecko.driver","C:\\Users\\vivekkumarsingh\\Desktop\\sel\\geeko\\geckodriver.exe");
-		//System.setProperty("webdriver.firefox.marionette","C:\\Users\\vivekkumarsingh\\Desktop\\sel\\geeko\\geckodriver.exe");
-		//comment the above 2 lines and uncomment below 2 lines to use Chrome
-    	//WebDriver driver2 = new ChromeDriver();
 		
     	System.setProperty("webdriver.chrome.driver","C:\\Users\\vivekkumarsingh\\Desktop\\sel\\ww\\chromedriver.exe");
-        //WebDriver driver = new FirefoxDriver();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://172.27.52.20/om_apm_qs/");
 
         
-        WebElement username = driver.findElement(By.name("wtUserNameInput"));
-        username.sendKeys("admin");
-        WebElement password = driver.findElement(By.name("wtPasswordInput"));
-        password.sendKeys("admin");
+        driver.findElement(By.name("wtUserNameInput")).sendKeys("admin");
+         driver.findElement(By.name("wtPasswordInput")).sendKeys("admin");
         driver.findElement(By.xpath("//a[contains(.,'Log on')]")).click();
        
-        // driver.findElement(By.xpath("//a[@class='dashboard-menu-entry ThemeGrid_Width4' or contains(text(), 'SSO')]")).click();
-       // driver.findElement(By.className("title")).click();
-       // driver.findElement(By.xpath("//*[contains(text(), '/om_apm_suntrust_ui/SSOScreen.jsf?Name=NEW')]")).click();
-       // driver.findElement(By.cssSelector("#om_apm_wt7/3a wtMainContent/3a wtPluginsLR/3a 7/3a wt2/a wt2")).click();
-      //  Thread.sleep(10);
-       // WebElement SSO = driver.findElement(By.id("om_apm_wt7:wtMainContent:wtPluginsLR:7:wt2:wt2"));
-        Thread.sleep(5000);
+        
+        WebDriverWait wait4 = new WebDriverWait(driver, 20);
+	       wait4.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(.,'Tests SSO Parameters simulation')]")));
+        
+        
+        
         driver.findElement(By.xpath("//a[contains(.,'Tests SSO Parameters simulation')]")).click();
-        WebElement bcin = driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_BusinessCIN"));
-        bcin.clear();
-        bcin.sendKeys("00440659456");
-        WebElement gcin = driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_GuarantorCIN"));
-        gcin.clear();
-        gcin.sendKeys("00440676062");
+        
+        WebDriverWait wait5 = new WebDriverWait(driver, 20);
+	       wait5.until(ExpectedConditions.presenceOfElementLocated(By.name("om_apm_wt39:wtMainContent:wtStsso_BusinessCIN")));
+        
+        driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_BusinessCIN")).clear();
+        driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_BusinessCIN")).sendKeys("00440659456");
+        driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_GuarantorCIN")).clear();
+        driver.findElement(By.name("om_apm_wt39:wtMainContent:wtStsso_GuarantorCIN")).sendKeys("00440676062");
         
         
-      WebElement submit =  driver.findElement(By.id("om_apm_wt39:wtMainContent:wt48"));
-      submit.click();
-      Thread.sleep(3000);
+      driver.findElement(By.id("om_apm_wt39:wtMainContent:wt48")).click();
+      Thread.sleep(500);
+      
+      //WebDriverWait wait11 = new WebDriverWait(driver, 300);
+      new WebDriverWait(driver, 300).until(ExpectedConditions.presenceOfElementLocated(By.name("om_apm_wt390:wtMainContent:wtAmlBsaPatriotDisClosure")));
+      driver.findElement(By.name("om_apm_wt390:wtMainContent:wtAmlBsaPatriotDisClosure")).click();
+     // WebDriverWait wait9 = new WebDriverWait(driver, 300);
+      new WebDriverWait(driver, 300).until(ExpectedConditions.presenceOfElementLocated(By.name("om_apm_wt390:wtMainContent:wtApplicationProductType")));
 
-      Select drpCountry = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplicationProductType")));
-		drpCountry.selectByVisibleText("Standard");
-		Thread.sleep(1000);
-		Select rewards = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplication_RewardId")));
-		rewards.selectByVisibleText("No Rewards");
-		Thread.sleep(1000);
+      //Select drpCountry = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplicationProductType")));
+      new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplicationProductType"))).selectByVisibleText("Standard");
+		Thread.sleep(500);
+		//Select rewards = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplication_RewardId")));
+		new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtApplication_RewardId"))).selectByVisibleText("No Rewards");
+		Thread.sleep(500);
         
-		WebElement bmail = driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_Email"));
-        bmail.sendKeys("test@mail.com");
-        WebElement bdesc = driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_BusinessDescription"));
-        bdesc.sendKeys("test@mail.com");
-        WebElement bphone = driver.findElement(By.name("om_apm_wt390:wtMainContent:wtBusinessContactPhone_PhoneNumber"));
-        bphone.sendKeys("1234567890");
-        Select bphtype = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtBusinessContactPhone_PhoneType")));
-        bphtype.selectByVisibleText("Business");
-//        driver.findElement(By.xpath("//*[@id="om_apm_wt390:wtMainContent:wt1239"]/span")).click();
-        /*WebElement copy = driver.findElement(By.id("om_apm_wt390:wtMainContent:wt1239"));copy.click();
-        driver.findElement(By.xpath("//span[contains(.,'Copy physical address')]")).click();*/
-        //WebElement elementToClick = driver.findElement(By.xpath("//*[@id="om_apm_wt390:wtMainContent:wt1239"]/span"));
+		driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_Email")).sendKeys("test@mail.com");
+        driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_BusinessDescription")).sendKeys("test@mail.com");
+        driver.findElement(By.name("om_apm_wt390:wtMainContent:wtBusinessContactPhone_PhoneNumber")).sendKeys("1234567890");
+        //Select bphtype = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtBusinessContactPhone_PhoneType")));
+        new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtBusinessContactPhone_PhoneType"))).selectByVisibleText("Business");
 
         WebElement elementToClick =driver.findElement(By.xpath("//*[contains(text(), 'Copy physical address')]"));
         ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick.getLocation().y+")");
         elementToClick.click();
-        //driver.findElement(By.xpath("//span[contains(.,'Copy physical address')]")).click();
-       // Thread.sleep(3000);
-       //WebElement bphone2 = driver.findElement(By.linkText("Copy physical address"));bphone2.click();
-        //driver.findElement(By.linkText("Copy physical address").click();
-        /*IJavaScriptExecutor ex = (IJavaScriptExecutor)Driver;
-        ex.ExecuteScript("arguments[0].click();", elementToClick);*/
+
+
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_AnnualSales")).sendKeys("234,444.44");
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSmallBusiness_EstimatedMonthlySpending")).sendKeys("23.44");
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_Phone")).sendKeys("1234567890");
-        Select gphtype = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_PhoneType")));
-        gphtype.selectByVisibleText("Business");
+        new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_PhoneType"))).selectByVisibleText("Business");
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_Email")).sendKeys("test@mail.com");
         
-        Select gmail = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_EmailAddressType")));
-        gmail.selectByVisibleText("Business");
+        new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_EmailAddressType"))).selectByVisibleText("Business");
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_title")).sendKeys("Mr");
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtguarantorAnnualIncomeDisclouse")).click();
         Thread.sleep(1000);
         driver.findElement(By.name("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_TotalAnnualIncome")).sendKeys("9000000");
         
-        String parentWindowHandler = driver.getWindowHandle();
-        Select org = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSTBankerSubmiiterInfo_Organization")));
-        org.selectByVisibleText("Branch Banker");
-        Select grp = new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSTBankerSubmiiterInfo_BankingGroup")));
-        grp.selectByVisibleText("Cross sell");
+        WebElement elementToClick6 =driver.findElement(By.id("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtGuarantor_DigitalCom"));
+        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick6.getLocation().y+")");
+        elementToClick6.click();
+       String parentWindowHandler= driver.getWindowHandle();
+        Thread.sleep(1000);
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src,'Disclosure_Popup.jsf?DisclosureType')]")));
+	     new WebDriverWait(driver, 300).until(ExpectedConditions.presenceOfElementLocated(By.id("om_apm_wt4:wtMainContent:wtClose")));
+	       driver.findElement(By.id("om_apm_wt4:wtMainContent:wt1")).click();
+	       driver.switchTo().window(parentWindowHandler);
+        new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSTBankerSubmiiterInfo_Organization"))).selectByVisibleText("Branch Banker");
+        new Select(driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSTBankerSubmiiterInfo_BankingGroup"))).selectByVisibleText("Cross sell");
         
         WebElement elementToClick2 =driver.findElement(By.xpath("//*[@id='om_apm_wt390:wtMainContent:wtFairCreditReportActDisclosur']"));
         ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick2.getLocation().y+")");
@@ -114,86 +99,50 @@ public class Automate {
         elementToClick3.click();
         
        driver.findElement(By.name("om_apm_wt390:wtMainContent:wtSearch_Officer")).click();
-       
-       for(String window : driver.getWindowHandles()) {
-    	   System.out.println(window);
-       driver.switchTo().window(window);
-       driver.manage().window().maximize();
-   }
-       Select code = new Select(driver.findElement(By.id("om_apm_wt47:wtMainContent:wtSTOfficer_SearchParamType")));
-		code.selectByVisibleText("Officer Id");
-		
+       Thread.sleep(1000);
+
+      
+       driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src,'Officer_Popup.jsf?branchCostCenterNumber')]")));
+       new WebDriverWait(driver, 300).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\'om_apm_wt47:wtMainContent:wt28\']")));
+       new Select(driver.findElement(By.id("om_apm_wt47:wtMainContent:wtSTOfficer_SearchParamType"))).selectByVisibleText("Officer Id");
 		
 		driver.findElement(By.id("om_apm_wt47:wtMainContent:wt28")).sendKeys("00038090");
-       // Thread.sleep(500);
-       // driver.findElement(By.name("om_apm_wt390:wtMainContent:wtOverarchingDisclosure")).click();
-        Thread.sleep(500);
-        
-     //   driver.findElement(By.id("om_apm_wt390:wtMainContent:wtCoApplicantsTable:0:wtInfoDigitalCommunication")).click();
-     //   driver.findElement(By.name("om_apm_wt390:wtMainContent:wtofficer")).click();
-        
-        
-        // Store your parent window
-       
-        System.out.println("parent "+parentWindowHandler);
-        /*for(String window : driver.getWindowHandles()) {
-            driver.switchTo().window(window);
-            driver.manage().window().maximize();
-        }*/
-       /* WebDriverWait wait = new WebDriverWait(driver, 300);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='om_apm_wt4:wtMainContent:wt1']")));*/
-        //
-     //   waitForNumberofWindowsToEqual(2);
-        Thread.sleep(3000);
-       /* String subWindowHandler = null;
+		driver.findElement(By.id("om_apm_wt47:wtMainContent:wt33")).click();
+		new WebDriverWait(driver, 3000).until(ExpectedConditions.presenceOfElementLocated(By.id("om_apm_wt47:wtMainContent:wtofficerRecords:0:wt12")));
+	       driver.findElement(By.id("om_apm_wt47:wtMainContent:wtofficerRecords:0:wt12")).click();
+	       Thread.sleep(3000);
 
-       Set<String> handles = driver.getWindowHandles(); // get all window handles
-       String lastWindow = (String) handles.toArray()[handles.size() - 1];
-       driver.switchTo().window(lastWindow);
-        System.out.println("size= "+handles.size());
-        Iterator<String> iterator = handles.iterator();
-       // driver.switchTo().frame(1);
-        
-        System.out.println("*********");
-       */
-        /*while (iterator.hasNext()){
-            subWindowHandler = iterator.next();
-                    }
-        driver.switchTo().window(subWindowHandler); // switch to popup window
-        System.out.println(subWindowHandler);*/
-        Thread.sleep(1000);
-        //driver.findElement(By.name("om_apm_wt390:wtMainContent:wtFairCreditReportActDisclosur")).click();
-       // driver.findElement(By.name("om_apm_wt390:wtMainContent:wtOverarchingDisclosure")).click();
-       /* WebElement elementToClick2 =driver.findElement(By.xpath("//*[contains(text(), 'Decline')]"));
-        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick2.getLocation().x+")");
+	       driver.findElement(By.id("om_apm_wt47:wtMainContent:wt31")).click();
+	       
+	       Thread.sleep(3000);
+	       driver.switchTo().window(parentWindowHandler);
 
-        elementToClick2.click();*/
-     //   WebDriverWait wait = new WebDriverWait(driver, 1);
-    //    wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("om_apm_wt4:wtMainContent:wtClose")));
-        
-        /*WebDriverWait wait = new WebDriverWait(driver, 300);
-        //wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='om_apm_wt4:wtMainContent:wt1']")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("om_apm_wt4:wtMainContent:wt1")));*/
-      //*[@id="om_apm_wt4:wtMainContent:wt1"]
-       // driver.findElement(By.xpath("//*[@id='om_apm_wt4:wtMainContent:wt1']")).click();
-     /*   WebElement elementToClick2 =driver.findElement(By.xpath("//*[contains(text(), 'Decline')]"));
-        //((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick2.getLocation().y+")");
-        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick2.getLocation().x+")");
+	       new WebDriverWait(driver, 3000).until(ExpectedConditions.presenceOfElementLocated((By.id("om_apm_wt390:wtMainContent:wtofficer"))));
+	       WebElement elementToClick4 =driver.findElement(By.id("om_apm_wt390:wtMainContent:wtofficer"));	       
 
-        elementToClick2.click();*/
-      //  driver.findElement(By.id("om_apm_wt4:wtMainContent:wt1")).click();
+	       ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick4.getLocation().y+")");
+	        elementToClick4.click();
+		       Thread.sleep(3000);
 
-        // Now you are in the popup window, perform necessary actions here
-        
-        //driver.switchTo().window(parentWindowHandler); 
-       // driver.switchTo().window(parentWindowHandler); 
-        /*WebElement elementToClick2 =driver.findElement(By.xpath("//*[contains(text(), 'Decline')]"));
-        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+elementToClick2.getLocation().y+")");
-        elementToClick2.click();*/
-        //driver.findElement(By.xpath("//a[contains(.,'Decline')]")).click();
-      //driver.findElement(By.id("om_apm_wt4:wtMainContent:wt1")).click();
-        //Decline
-       // om_apm_wt390:wtMainContent:wtSmallBusiness_AnnualSales
+	        
+	       driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src,'Originatingbranch_Popup.jsf?(Not.Licensed.For.Production)')]")));
+	       WebDriverWait wait6 = new WebDriverWait(driver, 3000);
+	       wait6.until(ExpectedConditions.presenceOfElementLocated(By.id("om_apm_wt15:wtMainContent:wt9")));
+	    		   driver.findElement(By.id("om_apm_wt15:wtMainContent:wt9")).sendKeys("0113");	     
+
+	    		   driver.findElement(By.id("om_apm_wt15:wtMainContent:wt33")).click();
+	       
+	    		   new WebDriverWait(driver, 10000).until(ExpectedConditions.presenceOfElementLocated(By.id("om_apm_wt15:wtMainContent:wtOriginationBranchRecords:0:wt3")));
+	    		   driver.findElement(By.id("om_apm_wt15:wtMainContent:wtOriginationBranchRecords:0:wt3")).click();
+	    		   Thread.sleep(2000);
+	    		   driver.findElement(By.id("om_apm_wt15:wtMainContent:wt12")).click();
+	    		   Thread.sleep(3000);
+	    	       driver.switchTo().window(parentWindowHandler);
+
+	    		  
+	    		   WebElement submit2 =  driver.findElement(By.id("om_apm_wt390:wtActions:wtsubmitBtn"));
+	    		      submit2.click();
+     
        //driver.close();
        
     }
